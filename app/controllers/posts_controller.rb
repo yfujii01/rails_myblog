aeef5a4bac63605ab0ff3c39c9168035dce81453
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-07-14 11:22:34 +0900

    add validation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,9 +16,12 @@ class PostsController < ApplicationController
 		# @post = Post.new(params[:post])
 		# @post = Post.new(params.require(:post).permit(:title, :body))
 		@post = Post.new(post_params)
-		@post.save
-		# redirect
-		redirect_to posts_path
+		if @post.save
+			# redirect
+			redirect_to posts_path
+		else
+			render plain: @post.errors.inspect
+		end
 	end
 
 	private
