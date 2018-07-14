29a601fb89f1a69d66d4be3317a6ba41484efeac
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-07-14 11:05:25 +0900

    add create program

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,5 +11,18 @@ class PostsController < ApplicationController
 	end
 
 	def create
+		# render plain: params[:post].inspect
+		# save
+		# @post = Post.new(params[:post])
+		# @post = Post.new(params.require(:post).permit(:title, :body))
+		@post = Post.new(post_params)
+		@post.save
+		# redirect
+		redirect_to posts_path
 	end
+
+	private
+		def post_params
+			params.require(:post).permit(:title, :body)
+		end
 end
