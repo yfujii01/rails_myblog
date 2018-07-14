011ed9cdc8715b2230452f0749b90d8a24669f3a
yfujii01 <y-fujii@rhizome-dev.sakura.ne.jp>
2018-07-14 13:55:40 +0900

    add comment form

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ApplicationRecord
   belongs_to :post
-  validates: :body, presence: true
+  validates :body, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-	has_many :coments
-
+	has_many :comments
+	
 	validates :title, presence: true, length: { minimum: 3, message: 'Too Short to post' }
 	validates :body, presence: true
 end
